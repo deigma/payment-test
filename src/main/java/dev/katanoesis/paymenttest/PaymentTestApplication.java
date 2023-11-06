e38964b2db82579f03b9c2aa8799e2a25b3866a0
J Kim <jkim987891@gmail.com>
2023-11-06 12:35:56 +0900

    Initial Commit

---
+++ src/main/java/dev/katanoesis/paymenttest/PaymentTestApplication.java
@@ -0,0 +1,13 @@
+package dev.katanoesis.paymenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentTestApplication.class, args);
+	}
+
+}
